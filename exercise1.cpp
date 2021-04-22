33ebe13485486c980d5f3030ab273296b2fb72f6
lifeiswonder <83015410+lifeiswonder@users.noreply.github.com>
2021-04-22 20:40:40 +0800

    Add files via upload
    test

---
+++ exercise1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() 
+{
+	int n=5, k=3;
+	cin>> n;
+	cin>> k;
+	int* str = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str[i];
+	}
+	return 0;
+}
